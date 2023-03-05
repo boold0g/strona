f401fe7ced7f8d13a777ce3aba10bb30bf3a0e18
boold0g <dominik.mirowski.24@gmail.com>
2023-03-05 22:24:09 +0100

    oi

--- src/Components/Projekty/Przycisk/Formularz/Formularz.js
+++ src/Components/Projekty/Przycisk/Formularz/Formularz.js
@@ -1,10 +1,47 @@
 import classes from "./Formularz.module.css";
+import { useRef } from "react";
 function Formularz(props) {
+  const tytulInputRef = useRef();
+  const opisInputRef = useRef();
+  const zdjecieInputRef = useRef();
   function addClose() {
     props.onCancel();
   }
+  function submitHandler(event) {
+    event.preventDefault();
+    const tytul = tytulInputRef.current.value;
+    const zdjecie = zdjecieInputRef.current.value;
+    const opis = opisInputRef.current.value;
+    const projektObject = {
+      tytul: tytul,
+      zdjecie: zdjecie,
+      opis: opis,
+    };
+    console.log(projektObject);
+  }
   return (
     <div id={classes.formularz}>
+      <form id={classes.formularz} onSubmit={submitHandler}>
+        <div className={classes.formItem}>
+          <label htmlFor="tytul">Tytuł</label>
+          <input type="text" required id="tytul" ref={tytulInputRef} />
+        </div>
+        <div className={classes.formItem}>
+          <label htmlFor="zdjecie">Zdjęcie</label>
+          <input type="url" required id="zdjecie" ref={zdjecieInputRef} />
+        </div>
+        <div className={classes.formItem}>
+          <label htmlFor="opis">Opis</label>
+          <textarea
+            rows="10"
+            cols="50"
+            id="opis"
+            name="opis"
+            ref={opisInputRef}
+          ></textarea>
+        </div>
+        <button id={classes.submit}>DODAJ</button>
+      </form>
       <button onClick={addClose} id={classes.exit}>
         X
       </button>
